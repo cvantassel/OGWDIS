ea9d1dba2bd8dc953d5a6d0e71226948a4ead69c
lvolkmann <landonvolkmann@gmail.com>
2019-11-15 13:49:35 -0600

    DB connection established

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+import mysql.connector as conn
+from app.config import config
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+
+    og_conn = conn.connect(**config)
+
+    cursor = og_conn.cursor()
+    
+    # cursor.execute("insert into test values('testval2');")
+    # og_conn.commit()
+
+    cursor.execute("SELECT * FROM test;")
+    
+    result = ""
+
+    for element in cursor:
+        result += element[0]
+
+    return result
